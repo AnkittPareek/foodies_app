3b38c08b0926016f1344beec958a3b0a9b8d8ce8
Ankit <akpareek4@gmail.com>
2024-02-04 19:09:29 +0530

    form to share meals

--- app/meals/[id]/page.js
+++ app/meals/[id]/page.js
@@ -1,3 +1,33 @@
-export default function ({ params }) {
-  return <h1>Meal: {params.id}</h1>;
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+export default function MealDetails({ params }) {
+  let meal = getMeal(params.id);
+  if (!meal) {
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>{" "}
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 }
